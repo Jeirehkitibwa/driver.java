91b281b465f0b03fe4175ba0bca69e3852b14ce3
jeremiah kitibwa <jeirehmazoe@gmail.com>
2025-06-11 17:21:29 +0300

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,29 @@
+package utms.encapsulation;
+
+public class Driver {
+    private String driverId;
+    private String name;
+    private String licenseNumber;
+
+    public Driver(String driverId, String name, String licenseNumber) {
+        this.driverId = driverId;
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getDriverId() {
+        return driverId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+}
